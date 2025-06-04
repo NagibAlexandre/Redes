a922bea6686fc4487327902988cf46b62df6966f
VitorLucioOliveira <vitor.lucio.0916@gmail.com>
2025-06-04 15:43:29 -0300

    teste de display para votar

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();//cria o frame
+       frame.setTitle("Trabalho Pratico Redes");//coloc o nome
+        frame.setVisible(true);
+
+        frame.setSize(500, 500);//decide tamanho e largura
+
+    }
+}
